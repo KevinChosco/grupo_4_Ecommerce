197c8f6352b0f1e835778bdd41208810d72456df
Kchosco20 <kevinchosco20@gmail.com>
2023-08-10 23:56:07 -0300

    Actualizando el header y el formulario de login

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const express = require("express")
 const app = express()
 const path = require("path")
 
+const port=process.env.PORT ?? 8080;
+
 app.use(express.static("public"))
 
-app.listen(3000, () => console.log("servidor escuchando en el puerto 3000"))
+app.listen(port, () => console.log("servidor escuchando en el puerto",port))
 
 app.get("/",(req,res)=>{
 res.sendFile(path.join(__dirname,"/views/home.html"))
