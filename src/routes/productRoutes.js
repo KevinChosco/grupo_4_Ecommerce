f4db837ae45ad516c0a137a93d966ee3e7888ebc
IgnacioGareca156 <nachosan9@gmail.com>
2023-09-13 13:00:40 -0300

    cambios en rutas y formulario crear

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,20 +3,23 @@ const path = require("path");
 const productControllers = require("../controllers/productControllers");
 const productRouter = express.Router();
 
+//MOSTRAR DETALLE DE UN PRODUCTO
 productRouter.get("/detail/:id", productControllers.detail);
 
+//MOSTRAR TODOS LOS PRODUCTOS
 productRouter.get("/", productControllers.index)
 
+//EDITAR PRODUCTO
 productRouter.get("/edit/:id", productControllers.edit)
-
 productRouter.post("/edit/:id", productControllers.update)
 
+//CREAR PRODUCTO
+productRouter.get("/create", productControllers.create)
+// productRouter.post("/create", productControllers)
+
+//ELIMINAR PRODUCTO
 productRouter.post("/delete/:id", productControllers.destroy)
-
-// productRouter.get("/detail", productControllers.showDetail);
-
-// productRouter.get("/detalle-producto.html", productControllers.showDetalleProduc);
-    
+   
 productRouter.get("/carrito-de-compras.html", productControllers.showCarrito);
     
 productRouter.get("/formulario-producto", productControllers.showFormularioProduc);
